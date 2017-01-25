b8ca88417318b7774e889676f6a407dceb85a253
coderxj <1134418544@qq.com>
2017-01-25 22:40:39 +0800

    Update LinkStack.java

--- JAVA/LinkStack.java
+++ JAVA/LinkStack.java
@@ -1,23 +1,23 @@
-
+package acme.list;
 /*
 * author: acme
 *   date: 2017-1-22
 *  blogs: http://blog.csdn.net/qq_18297675
+* update: 2017-1-25
+*   info: change node class become inner private class
 */
 public class LinkStack<T> {  //链栈类
-
-	public static void main(String[] args) throws Exception {
-
-		LinkStack<Character> stack = new LinkStack<>();
-		for (int i = 0; i < 10; i++) {
-			stack.push((char)(i+65));
+	
+	class Node {  //节点类
+		
+		public T data;
+		public Node next;
+		
+		public Node(T d) {
+			data = d;
 		}
-		stack.pop();
-		//System.out.println();
-		stack.print();
 	}
-
-	private Node<T> top;   //栈顶指针
+	private Node top;   //栈顶指针
 	private int count;     //记录栈的大小
 	
 	public LinkStack() {
@@ -26,7 +26,7 @@ public class LinkStack<T> {  //链栈类
 	
 	//初始化栈
 	private void initStack() {  
-		top = new Node(0);  //创建栈顶指针
+		top = new Node(null);  //创建栈顶指针
 		count = 0;
 	}
 	//栈是否为空
@@ -41,8 +41,7 @@ public class LinkStack<T> {  //链栈类
 	
 	//入栈
 	public void push(T e) {       
-		Node<T> node = new Node(0);  //创建一个新的节点
-		node.data = e;
+		Node node = new Node(e);  //创建一个新的节点
 		node.next = top.next;  
 		top.next = node;          //让栈顶指针指向新的栈顶节点
 		++count;
@@ -50,7 +49,7 @@ public class LinkStack<T> {  //链栈类
 		
 	//出栈(不返回栈顶元素)
 	public void pop() throws Exception{          
-		if(isEmpty()) {  //如果栈为空，就什么也不做
+		if(isEmpty()) {  
 			throw new Exception("栈为空，出栈操作失败.");
 		} else {
 			top.next = top.next.next;
@@ -60,7 +59,7 @@ public class LinkStack<T> {  //链栈类
 	
 	//出栈(返回栈顶元素)
 	public T popE() throws Exception{            
-		if(isEmpty()) {  //如果栈为空，就什么也不做
+		if(isEmpty()) {  
 			throw new Exception("栈为空，出栈操作失败.");
 		} else {
 			T data = top.next.data; //先把要返回的值保存下来
@@ -72,10 +71,10 @@ public class LinkStack<T> {  //链栈类
 		
 	//获取栈顶元素
 	public T getTop() throws Exception{           
-		if(isEmpty()) {  //如果栈为空，就什么也不做
+		if(isEmpty()) {  
 			throw new Exception("栈为空，获取栈顶元素操作失败.");
 		} else {
-			return (T)top.next.data;
+			return top.next.data;
 		}
 	}
 		
@@ -84,7 +83,7 @@ public class LinkStack<T> {  //链栈类
 		if(isEmpty()) {  //如果栈为空，就什么也不做
 			throw new Exception("栈为空，遍历栈操作失败.");
 		} else {
-			Node<T> cur = top;    //和链表遍历一样
+			Node cur = top;    //和链表遍历一样
 			while ((cur = cur.next) != null)  
 				System.out.println(cur.data);
 		}
@@ -99,18 +98,5 @@ public class LinkStack<T> {  //链栈类
 				pop();
 			top.next = null;
 		}
-	}
-	
-	
-}
-
-class Node<T> {  //节点类
-	
-	public T data;
-	public Node<T> next;
-	
-	public Node(T d) {
-		data = d;
-		next = null;
 	}
 }
